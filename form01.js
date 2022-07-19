dd87a3f2bf9d19f828b8cfe24161afce9b0998d0
LucasPaivaSantos <lucaspaiva@live.com>
2022-07-19 20:57:25 -0300

    extrai estado da interface

--- form01.js
+++ form01.js
@@ -1,6 +1,13 @@
+let estado = {
+  cor: "#000000",
+  altura: 0,
+  largura: 0,
+};
+
 function criaElemento(cor, largura, altura) {
   const eNovo = document.createElement("div");
   eNovo.textContent = `${cor} ${largura} ${altura}`;
+  eNovo.style = `color: ${cor}`;
   document.body.appendChild(eNovo);
 }
 
@@ -8,11 +15,12 @@ document.exemplo01.addEventListener("submit", submitListener);
 
 function submitListener(event) {
   event.preventDefault();
-  criaElemento(
-    document.exemplo01.cor.value,
-    Number(document.exemplo01.largura.value),
-    Number(document.exemplo01.altura.value)
-  );
+
+  (estado.cor = document.exemplo01.cor.value),
+    (estado.largura = Number(document.exemplo01.largura.value)),
+    (estado.altura = Number(document.exemplo01.altura.value));
+
+  criaElemento(estado.cor, estado.largura, estado.altura);
 
   document.exemplo01.reset();
 }
